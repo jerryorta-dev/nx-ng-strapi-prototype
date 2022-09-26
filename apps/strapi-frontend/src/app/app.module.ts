eb7a426fb8ad71ae769cf4410a477340f35532c5
Jerry Orta <jorta@americanfirstfinance.com>
2022-09-26 12:27:37 -0500

    Create nx-ng-strapi-prototype with strapi-frontend app

--- apps/strapi-frontend/src/app/app.module.ts
+++ apps/strapi-frontend/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, StoreModule.forRoot({}, {}), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }), EffectsModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ libs/any-shared/secrets/README.md
@@ -0,0 +1,7 @@
+# any-shared-secrets
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test any-shared-secrets` to execute the unit tests.
